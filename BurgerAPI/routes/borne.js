dd88d07edaa4d9cb91392359baf054556c7dd5c8
Jean <jdeyehe@gmail.com>
2018-04-09 18:43:56 +0200

    add New ingredient

--- BurgerAPI/routes/borne.js
+++ BurgerAPI/routes/borne.js
@@ -38,6 +38,39 @@ const newProduit = BorneController.createProduit(nameP,description,ingredients);
 res.status(201).json(newProduit);
 });
 
+borneRouter.get('/allProduct' , function(req,res){
+  const products = BorneController.getAllProduct();
+  res.json(products);
+
+});
+
+borneRouter.get('/allIngredient/:productName' , function(req,res){
+  const ingredients = BorneController.getAllIngredientByProduct(req.params.productName)
+  res.json(ingredients);
+});
+
+
+borneRouter.post('/:productName/addIngredient', function(req, res) {
+
+  const name = req.body.name;
+  const type = req.body.type;
+  const quantite = parseInt(req.body.quantite);
+  const ingredients = BorneController.getAllIngredientByProduct(req.params.productName)
+
+  if(name === undefined || type === undefined || quantite === undefined || ingredients === undefined ) {
+    res.status(400).end();
+    return;
+  }
+
+  const i = BorneController.createIngredient(name, type , quantite);
+
+
+
+      return;
+
+  res.status(404).end();
+});
+
 // Fin
 
 module.exports = borneRouter;
