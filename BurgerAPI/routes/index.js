40af6324cf83caeca42957807515a31f9929ed03
jdeyehe01 <34164147+jdeyehe01@users.noreply.github.com>
2018-04-26 17:17:24 +0200

    Add files via upload

--- BurgerAPI/routes/index.js
+++ BurgerAPI/routes/index.js
@@ -1,7 +1,7 @@
+
 const RouteManager = function() { };
 
 RouteManager.attach = function(app) {
-
   app.use('/menu' , require('./menu'));
   app.use('/ingredient', require('./ingredient'));
   app.use('/product', require('./product'));
